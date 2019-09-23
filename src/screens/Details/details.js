8cf739e554cd93fc5b425f131d16fc7de10867df
souro89 <souro89@gmail.com>
2019-09-23 19:39:40 +0400

    Added restaurant Image

--- src/screens/Details/details.js
+++ src/screens/Details/details.js
@@ -1,16 +1,67 @@
 import React, { Component } from "react";
 import "./details.css";
 import Header from "../../common/Header";
+import { Grid } from "@material-ui/core";
 
 class Details extends Component {
   constructor() {
     super();
+    this.state = {
+      restaurantDetails: {
+        address: "",
+        average_price: "",
+        categories: "",
+        customer_rating: "",
+        id: "",
+        number_customers_rated: "",
+        photo_URL: "",
+        restaurant_name: ""
+      }
+    };
+  }
+
+  componentWillMount() {
+    sessionStorage.removeItem("customer-cart");
+
+    let that = this;
+    let data = null;
+    let xhr = new XMLHttpRequest();
+    xhr.addEventListener("readystatechange", function() {
+      if (this.readyState === 4) {
+        that.setState({
+          restaurantDetails: JSON.parse(this.responseText)
+        });
+      }
+    });
+    xhr.open(
+      "GET",
+      `${this.props.baseURL}/restaurant/${this.props.match.params.id}`
+    );
+    xhr.setRequestHeader("Cache-Control", "no-cache");
+    xhr.send(data);
   }
 
   render() {
     return (
       <div>
         <Header></Header>
+        <div>
+          <Grid
+            container
+            spacing={22}
+            className="backGroundGrey mobile-text-center"
+          >
+            <Grid item xs={12} sm={3} className="text-center">
+              <img
+                src={this.state.restaurantDetails.photo_URL}
+                width="300"
+                alt={this.state.photo_URL}
+                height="250"
+                className="m-t-20"
+              ></img>
+            </Grid>
+          </Grid>
+        </div>
       </div>
     );
   }
