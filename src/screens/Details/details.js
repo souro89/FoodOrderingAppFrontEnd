b5ceed2104d049cf922b75b5e60778470feef303
souro89 <souro89@gmail.com>
2019-09-24 19:48:59 +0400

    Added Checkout tab

--- src/screens/Details/details.js
+++ src/screens/Details/details.js
@@ -1,12 +1,28 @@
 import React, { Component } from "react";
 import "./details.css";
 import Header from "../../common/Header";
-import { Grid } from "@material-ui/core";
+import {
+  Grid,
+  Divider,
+  IconButton,
+  Card,
+  CardContent,
+  Typography,
+  Badge,
+  Button
+} from "@material-ui/core";
+import AddIcon from "@material-ui/icons/Add";
+import ShoppingCart from "@material-ui/icons/ShoppingCart";
 
 class Details extends Component {
   constructor() {
     super();
     this.state = {
+      open: false,
+      totalAmount: 0.0,
+      noOfItemsCart: 0,
+      snackBarText: "",
+      cartItems: [],
       restaurantDetails: {
         address: "",
         average_price: "",
@@ -58,6 +74,96 @@ class Details extends Component {
     });
   };
 
+  getItemList = () => {
+    let categories = this.state.restaurantDetails.categories;
+
+    if (categories.length <= 0) {
+      return;
+    }
+
+    return categories.map((item, index) => {
+      return (
+        <div className="mt-15" key={"item" + item.id}>
+          <div>{item.category_name}</div>
+          <Divider className="m-10"></Divider>
+          {this.getEachMenuItem(item.item_list, item.category_name)}
+        </div>
+      );
+    });
+  };
+
+  getEachMenuItem = (item_list, category_name) => {
+    return item_list.map((item, index) => {
+      item = { item, category_name: category_name };
+      console.log(item.item);
+      return (
+        <div className="flex pd-1-per" key={index}>
+          <div className="flex-5">
+            <i
+              className={
+                item.item.item_type === "NON_VEG"
+                  ? "fa fa-circle red"
+                  : "fa fa-circle green"
+              }
+            ></i>
+          </div>
+          <div className="flex-75">{item.item.item_name}</div>
+          <div className="flex-10">
+            <i className="fa fa-inr"></i> {item.item.price}.00
+          </div>
+          <div className="flex-10 plus-btn">
+            <IconButton
+              aria-label="Add"
+              style={{ padding: "1px" }}
+              onClick={() => this.addCheckoutList(item, "+")}
+            >
+              <AddIcon />
+            </IconButton>
+          </div>
+        </div>
+      );
+    });
+  };
+
+  addCheckoutList = (i, m) => {
+    let currentSelection, newcartItems;
+    let alreadyPresent = this.state.cartItems.filter(
+      c => i.item_name === c.item_name && i.category_name === c.category_name
+    );
+    if (alreadyPresent > 0) {
+      currentSelection = this.state.cartItems.map(item => {
+        if (
+          item.id === alreadyPresent[0].id &&
+          item.category_name === alreadyPresent[0].category_name
+        ) {
+          let count = item.count + 1;
+          item.count = count;
+          item.totalItemPrice = item.price * count;
+        }
+        return item;
+      });
+      newcartItems = [...currentSelection];
+    } else {
+      let count = alreadyPresent.length + 1;
+      let currentSelection = {
+        ...i,
+        count: count,
+        totalItemPrice: i.price * count
+      };
+      newcartItems = [...this.state.cartItems, currentSelection];
+    }
+
+    const noOfItemsCart = this.state.noOfItemsCart + 1;
+    const totalAmount = this.state.totalAmount + i.price;
+    this.setState({
+      cartItems: newcartItems,
+      open: true,
+      snackBarText: m,
+      noOfItemsCart: noOfItemsCart,
+      totalAmount: totalAmount
+    });
+  };
+
   render() {
     return (
       <div>
@@ -84,7 +190,66 @@ class Details extends Component {
                   <h3>{this.state.restaurantDetails.address.locality}</h3>
                   <h3>{this.getCategory()}</h3>
                 </Grid>
+                <Grid item xs={6} sm={6}>
+                  <div className="divcustomerRating">
+                    <i className="fa fa-star"></i>{" "}
+                    {this.state.restaurantDetails.customer_rating}
+                    <p className="text-style">
+                      AVERAGE RATING BY <br />{" "}
+                      <span style={{ fontWeight: "bold" }}>
+                        {this.state.restaurantDetails.number_customers_rated}
+                      </span>{" "}
+                      CUSTOMERS
+                    </p>
+                  </div>
+                </Grid>
+                <Grid item xs={6} sm={6}>
+                  <div className="divcustomerRating">
+                    <i className="fa fa-inr"></i>{" "}
+                    {this.state.restaurantDetails.average_price}
+                    <p className="text-style">
+                      AVERAGE COST FOR <br /> TWO PEOPLE
+                    </p>
+                  </div>
+                </Grid>
               </Grid>
+            </Grid>
+          </Grid>
+          <Grid container spacing={24} className="items">
+            <Grid item xs={12} sm={7}>
+              {this.getItemList()}
+            </Grid>
+            <Grid item xs={12} sm={5}>
+              <Card>
+                <CardContent>
+                  <Typography variant="h5" color="default" gutterBottom>
+                    <Badge
+                      badgeContent={this.state.noOfItemsCart}
+                      color="primary"
+                    >
+                      <ShoppingCart />
+                    </Badge>
+                    <span className="my-cart">My Cart</span>
+                  </Typography>
+
+                  {}
+                  <div className="bold pd-1-per">
+                    TOTAL AMOUNT{" "}
+                    <span className="right mr-8">
+                      <i className="fa fa-inr"></i> {this.state.totalAmount}.00
+                    </span>
+                  </div>
+                  <Button
+                    className="mt-24-px"
+                    variant="contained"
+                    fullWidth
+                    size="medium"
+                    color="primary"
+                  >
+                    CHECKOUT
+                  </Button>
+                </CardContent>
+              </Card>
             </Grid>
           </Grid>
         </div>
